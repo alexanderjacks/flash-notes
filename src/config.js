68f69a9b237ef2350e3dd18d3f72427c37c71291
Alexander Jacks <alexanderthejacks@gmail.com>
2020-01-07 17:02:48 -0800

    :mailbox_with_mail: connect Amplify backend

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: 'us-west-2',
+    BUCKET: 'dec2-sls-notes-app'
+  },
+  apiGateway: {
+    REGION: 'us-west-2',
+    URL: 'https://gcri6wqb59.execute-api.us-west-2.amazonaws.com/prod'
+  },
+  cognito: {
+    REGION: 'us-west-2',
+    USER_POOL_ID: 'us-west-2_bk1hmMXRj',
+    APP_CLIENT_ID: '4bp3dgav4belivtatnbekcrftp',
+    IDENTITY_POOL_ID: 'us-west-2:3726cc06-ae40-46f2-ac08-db94f91eaa37'
+  }
+};
